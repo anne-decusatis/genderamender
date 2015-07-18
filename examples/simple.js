14260d7828c21da83b5b7f36aa236e179d010fe5
Anne DeCusatis <annie.decusatis@gmail.com>
2015-07-18 14:36:59 -0400

    messy copying, but autocomplete works

--- examples/simple.js
+++ examples/simple.js
@@ -1,12 +1,30 @@
-// constructs the suggestion engine
-var genders = new Bloodhound({
-  datumTokenizer: Bloodhound.tokenizers.whitespace,
-  queryTokenizer: Bloodhound.tokenizers.whitespace,
-  prefetch: '../genders.txt'
-});
- 
-$('#gender-input .typeahead').typeahead(null,
-{
-  name: 'genders',
-  source: genders
+var gendermap = [
+"male",
+"female",
+"trans",
+"genderqueer",
+"agender",
+"cis"
+];
+
+$( document ).ready(function() {
+	var genders = new Bloodhound({
+		datumTokenizer: Bloodhound.tokenizers.whitespace,
+		queryTokenizer: Bloodhound.tokenizers.whitespace,
+		//prefetch: '../genders.json',
+		local: gendermap
+	});
+	$('#gender-input .typeahead').typeahead({
+		hint: true,
+		highlight: true,
+		minLength: 1
+	},
+	{
+		name: 'genders',
+		source: genders
+	});
+	
+	$('.typeahead').bind('typeahead:open', function(ev, suggestion) {
+	  console.log('opened');
+	});
 });
