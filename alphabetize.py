449aad37850a4e4aa61a099433cab752786fe80a
Anne DeCusatis <annie.decusatis@gmail.com>
2015-11-05 18:21:50 -0500

    fix alphabetize

--- alphabetize.py
+++ alphabetize.py
@@ -2,13 +2,10 @@ import json
 
 def alphabetize(filepath="./genders.json"):
 	genders = {}
-	sorted_genders = {}
 	with open(filepath, "r") as f:
 		genders = json.load(f)
 	with open(filepath, "w") as f:
-		for g in g.keys():
-			sorted_genders[g] = genders[g]
-		f.write(json.dumps(sorted_genders, indent = 2, ensure_ascii=False) + "\n")
+		f.write(json.dumps(genders, indent = 2, ensure_ascii=False, sort_keys=True) + "\n")
 		
 
 if __name__ == "__main__":
